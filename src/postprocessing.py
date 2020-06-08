18e99fa9673c0ec80ea347d3be728f1c60bd756a
echen01 <ericmchen1@gmail.com>
2020-06-07 20:06:18 -0400

    paste

---
+++ src/postprocessing.py
@@ -0,0 +1,20 @@
+from display import *
+from matrix import *
+from gmath import *
+from draw import *
+
+def nn_upscale(x, y, k):
+    pass
+
+def linear_upscale(x, y):
+    pass
+
+def bilinear_upscale(x,y):
+    pass
+
+def bicubic_upscale(x,y):
+    pass
+
+#Anti aliasing methods
+def super_sample(n):
+    pass
